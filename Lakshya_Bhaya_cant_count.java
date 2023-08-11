321cab36c862e8e29d5d4de84ad39580a783809b
Astha Arya <aasthaarya039@gmail.com>
2023-08-11 15:39:26 +0530

    counting

---
+++ Lakshya_Bhaya_cant_count.java
@@ -0,0 +1,13 @@
+package prepare;
+
+import java.util.Scanner;
+
+public class Lakshya_Bhaya_cant_count {
+	public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        for(int i=1;i<=num;i++){
+            System.out.println(i);
+        }
+    }
+}
